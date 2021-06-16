a678188062f5aef4fc460b50fcd75760bcd5465e
mbimbij <joseph.mbimbi@gmail.com>
2021-06-16 12:28:37 +0200

    Remove the failing test

--- src/test/java/fr/xebia/clickcount/MyTest.java
+++ src/test/java/fr/xebia/clickcount/MyTest.java
@@ -6,10 +6,10 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MyTest {
-  @Test
-  void failingTest() {
-    assertThat(true).isFalse();
-  }
+//  @Test
+//  void failingTest() {
+//    assertThat(true).isFalse();
+//  }
 
 
   @Test
