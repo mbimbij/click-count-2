76969469cdf1a6e79e9c8d09c61e714e769b55cf
mbimbij <joseph.mbimbi@gmail.com>
2021-06-17 06:48:54 +0200

    Execute Helm from travis

--- deployment/travis-deploy.sh
+++ deployment/travis-deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-echo "coucou"
-aws sts get-caller-identity --output text
-aws eks update-kubeconfig --name click-count-staging
+if [ -z $1 ]; then
+  echo -e "usage:\n./travis-deploy.sh \$ENVIRONMENT"
+  exit 1
+fi
+
+ENVIRONMENT=$1
+
+aws eks update-kubeconfig --name click-count-$ENVIRONMENT
 kubectl get nodes
 ls
